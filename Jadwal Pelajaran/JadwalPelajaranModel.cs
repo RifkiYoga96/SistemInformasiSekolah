677b65672b5aec683aeb1863be342a4a0334389a
Rifki Yoga <rys9612345@gmail.com>
2024-10-02 16:00:41 +0700

    frr

---
+++ Jadwal Pelajaran/JadwalPelajaranModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemInformasiSekolah.Jadwal_Pelajaran
+{
+    public class JadwalPelajaranModel
+    {
+        public int JadwalPelajaranId { get; set; }
+        public int KelasId { get; set; }
+        public string KelasName { get; set; }
+
+        public string Hari { get; set; }
+        public string JenisJadwal { get; set; }
+        public string JamMulai { get; set; }
+        public string JamSelesai { get; set; }
+
+        public int MapelId { get; set; }
+        public string MapelName { get; set; }
+        public int GuruId { get; set; }
+        public string GuruName { get; set; }
+        public string Keterangan { get; set; }
+    }
+}
